94167ddde0a3b22b6050c9d103f54b11ac51f09d
yeoneum <52909529+timevoltex@users.noreply.github.com>
2020-05-13 18:48:20 +0900

    Create Add.tsx

---
+++ Component/Add/Add.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text, StyleSheet, Button} from 'react-native'
+
+export default function Add(){
+  return (
+    <View>
+      <Text>This is Add Tab</Text>
+    </View>
+  )
+}
