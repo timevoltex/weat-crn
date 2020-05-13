f361d6b1d5b15669768b1bde53386d595b03b68c
yeoneum <52909529+timevoltex@users.noreply.github.com>
2020-05-13 18:48:51 +0900

    20.05.13 Svg 파일 모음집
    Svg 파일을 한곳으로 몰아서 import하기 용이하게 설정

---
+++ Component/AllImage.tsx
@@ -0,0 +1,282 @@
+import React from 'react';
+import Svg, { Path, Circle } from "react-native-svg";
+
+
+export const Bookmark_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path fill={props.fill} d="M23 26l-9-9-9 9V2h18v24z" />
+  </Svg>
+);
+
+export const Back_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      fill={props.fill}
+      d="M26 16H9.66l4.17 4.17L11 23l-9-9 9-9 2.83 2.83L9.66 12H26v4z"
+    />
+  </Svg>
+);
+
+export const Callender_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M25 5v4H3V5h4V3h2v2h10V3h2v2h4zM3 11v14h22V11zm7.7 11A3.4 3.4 0 018 20.93l.87-1.19a2.46 2.46 0 001.64.76c.73 0 1.21-.29 1.21-.84s-.36-1-2-1v-1.37c1.35 0 1.77-.4 1.77-1s-.31-.8-.9-.8a2.08 2.08 0 00-1.41.66L8.21 15a3.75 3.75 0 012.45-1c1.63 0 2.7.78 2.7 2.13a1.81 1.81 0 01-1.26 1.7v.05a1.9 1.9 0 011.51 1.86c0 1.48-1.34 2.26-2.91 2.26zm9.3-.15h-5v-1.5h1.7v-4.27h-1.45v-1.17a5.5 5.5 0 001.94-.72h1.39v6.16H20z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+
+export const Card_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M2 11v10a2 2 0 002 2h20a2 2 0 002-2V11zm8 9H5v-2h5zM26 7v1H2V7a2 2 0 012-2h20a2 2 0 012 2z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Coupon_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Circle cx={10} cy={11} r={1} fill={props.fill} />
+    <Path
+      d="M24.36 14l1.46-2.08-2.09-1.47.66-2.45-2.46-.66-.22-2.53-2.53.19-1.08-2.28-2.3 1.08L14 2l-1.8 1.8-2.3-1.08L8.82 5l-2.53-.19-.22 2.53L3.61 8l.66 2.45-2.09 1.47L3.64 14l-1.46 2.08 2.09 1.47L3.61 20l2.46.66.22 2.53L8.82 23l1.08 2.28 2.3-1.08L14 26l1.8-1.8 2.3 1.08L19.18 23l2.53.22.22-2.53 2.46-.69-.66-2.45 2.09-1.47zM7 11a3 3 0 113 3 3 3 0 01-3-3zm4 8H8l9-10h3zm7 1a3 3 0 113-3 3 3 0 01-3 3zm0-4a1 1 0 101 1 1 1 0 00-1-1z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Crown_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M18 11l-4-8-4 8-7-3 4 17h14l4-17zm-4 10a3 3 0 113-3 3 3 0 01-3 3z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Diner_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M3 3h6v10H3zm20 12v10h-6v-4a3 3 0 00-6 0v4H5V15zM11 3h6v10h-6zm8 0h6v10h-6z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Event_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M14 2a12 12 0 1012 12A12 12 0 0014 2zm1 19h-2v-2h2zm0-4h-2V7h2z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Feed_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M3 3v18h8.34L14 25l2.66-4H25V3zm4 12h8v2H7zm14-2H7v-2h14zm0-4H7V7h14z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Filter_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M21 12.5h4v3h-4v2h-3v-7h3v2zM10 18h3v7h-3v-2H3v-3h7v-2zm2-13h13v3H12zm3 15h10v3H15zM7 8H3V5h4V3h3v7H7V8zm-4 4.5h13v3H3z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Gps_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M22.78 12A9.05 9.05 0 0016 5.22V2h-4v3.22A9.05 9.05 0 005.22 12H2v4h3.22A9.05 9.05 0 0012 22.78V26h4v-3.22A9.05 9.05 0 0022.78 16H26v-4zM14 19a5 5 0 115-5 5 5 0 01-5 5z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Heart_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M26 10a2.71 2.71 0 010 .29 1.55 1.55 0 010 .22 1.24 1.24 0 010 .27C25.25 18.7 14 25 14 25S2.75 18.7 2.05 10.78a1.24 1.24 0 010-.27 1.55 1.55 0 010-.22A2.71 2.71 0 012 10v-.72c0-.25.07-.49.11-.72a6 6 0 015.58-5.49A6 6 0 018.55 3a4.16 4.16 0 01.54 0A6.48 6.48 0 0114 6.82 6.48 6.48 0 0118.91 3a4.16 4.16 0 01.54 0 6 6 0 01.86.07 6 6 0 015.55 5.49c0 .23.09.47.11.72s.03.48.03.72z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Home_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path d="M25 10.34V25h-7v-7a4 4 0 00-8 0v7H3V10.34L14 3z" fill={props.fill} />
+  </Svg>
+);
+export const List_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M2 5h4v4H2zm0 7h4v4H2zm0 7h4v4H2zM9 5h17v4H9zm0 7h17v4H9zm0 7h17v4H9z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Location_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M14 2a9 9 0 00-9 9 7.09 7.09 0 000 .92C5.68 19.53 14 26 14 26s8.32-6.47 9-14.08c0-.1.05-.71.05-.92A9 9 0 0014 2zm0 5a4 4 0 11-4 4 4 4 0 014-4z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+
+export const Locationmark_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M21.91 18.38a2.15 2.15 0 01-.18.23l-.13.14-.14.16-.31.33c-2 2.11-6.9 6.75-6.9 6.75l-1.17-3.84A10.11 10.11 0 015 7.59l4.25 2.83A1.79 1.79 0 0110 12a3.69 3.69 0 000 .47 4 4 0 102.32-3.95 1.78 1.78 0 01-1.74-.15L6.36 5.5A10.05 10.05 0 0114.19 2a10.1 10.1 0 017.72 16.38z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Marketing_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M21 4v18h-4a4 4 0 00-.08-.85 4.57 4.57 0 00-.39-1.27l-.13-.26A5 5 0 0012 17v7H8V9h4a5 5 0 004.4-2.62l.13-.26a4.57 4.57 0 00.39-1.27A4 4 0 0017 4z"
+      fill={props.fill}
+    />
+    <Path
+      d="M16.92 21.15a6.17 6.17 0 00-.39-1.27 4.57 4.57 0 01.39 1.27zm0-16.3a4.57 4.57 0 01-.39 1.27 6.17 6.17 0 00.39-1.27zM26 13a3 3 0 01-3 3v-6a3 3 0 013 3zM6 9v8H4a2 2 0 01-2-2v-4a2 2 0 012-2z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+
+export const Medal_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M25 3h-5l-4.14 6.22a8 8 0 00-3.72 0L8 3H3l5.48 8.21a8 8 0 1011 0zM14 21a4 4 0 01-4-4 4 4 0 013.29-3.93 3.64 3.64 0 011.42 0A4 4 0 0118 17a4 4 0 01-4 4z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Menu_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M2 21h24v2H2zm22-4v2H4v-2a10 10 0 019-9.95V5h2v2.05A10 10 0 0124 17z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Minus_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M14 2a12 12 0 1012 12A12 12 0 0014 2zm8 14H6v-4h16z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Multiply_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M22.49 5.51a12 12 0 100 17 12 12 0 000-17zm-1.42 12.73l-2.83 2.83L14 16.83l-4.24 4.24-2.83-2.83L11.17 14 6.93 9.76l2.83-2.83L14 11.17l4.24-4.24 2.83 2.83L16.83 14z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Mypage_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Circle cx={14} cy={9} r={6} fill={props.fill} />
+    <Path d="M25 25H3c0-4.42 4.92-8 11-8s11 3.58 11 8z" fill={props.fill} />
+  </Svg>
+);
+
+export const Notice_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M24 16v4H4v-4a3 3 0 003-3V9a7 7 0 0114 0v4a3 3 0 003 3zm-6 6a4 4 0 01-8 0z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Orderlist_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M24.87 6A4 4 0 0021 3H7a4 4 0 00-4 4v18h18V7h4a3.66 3.66 0 00-.13-1zM9 21H7v-2h2zm0-4H7v-2h2zm0-4H7v-2h2zm8 8h-6v-2h6zm0-4h-6v-2h6zm0-4h-6v-2h6zm0-4H7V7h10z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Plus_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M14 2a12 12 0 1012 12A12 12 0 0014 2zm8 14h-6v6h-4v-6H6v-4h6V6h4v6h6z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Quest_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M14 2a12 12 0 1012 12A12 12 0 0014 2zm-2 4h4v10h-4zm4 16h-4v-4h4z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Review_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M3 20v5h5l12-12-5-5L3 20zM16.997 5.996l2.998-2.998 5 5-2.999 2.997z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+
+export const Search_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M19.61 16.78A8.89 8.89 0 0021 12a9 9 0 10-9 9 8.89 8.89 0 004.78-1.39L22.17 25 25 22.17zM7 12a5 5 0 115 5 5 5 0 01-5-5z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Setting_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M26 16v-4l-3.41-.68a8.94 8.94 0 00-.59-1.5l1.9-2.89-2.83-2.83L18.18 6a8.94 8.94 0 00-1.5-.62L16 2h-4l-.68 3.41a8.94 8.94 0 00-1.5.59L6.93 4.1 4.1 6.93 6 9.82a8.94 8.94 0 00-.62 1.5L2 12v4l3.41.68a8.94 8.94 0 00.59 1.5l-1.9 2.89 2.83 2.83L9.82 22a8.94 8.94 0 001.5.62L12 26h4l.68-3.41a8.94 8.94 0 001.5-.62l2.89 1.93 2.83-2.83-1.9-2.89a8.94 8.94 0 00.62-1.5zm-12 2a4 4 0 114-4 4 4 0 01-4 4z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Stamp_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M6 24h16v2H6zm10-8v-4.42a5 5 0 10-4 0V16l-7 3v4h18v-4z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Ticket_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Circle cx={10} cy={11} r={1} fill={props.fill} />
+    <Path
+      d="M26 7V5H2v2a1 1 0 010 2v2a1 1 0 010 2v2a1 1 0 010 2v2a1 1 0 010 2v2h24v-2a1 1 0 010-2v-2a1 1 0 010-2v-2a1 1 0 010-2V9a1 1 0 010-2zM7 11a3 3 0 113 3 3 3 0 01-3-3zm1 8l9-10h3l-9 10zm10 1a3 3 0 113-3 3 3 0 01-3 3zm0-4a1 1 0 101 1 1 1 0 00-1-1z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Time_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M14 2a12 12 0 1012 12A12 12 0 0014 2zm7 16h-9V6h4v8h5z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+export const Trophy_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M16 21v-3.35A6 6 0 0020 12V3H8v9a6 6 0 004 5.65V21H9l-2 2v2h14v-2l-2-2zM3 5h3v7H3zm19 0h3v7h-3z"
+      fill={props.fill}
+    />
+  </Svg>
+);
+
+export const Weatfill_01 = props => (
+  <Svg viewBox="0 0 28 28" width={28} height={28} {...props}>
+    <Path
+      d="M24.1 12.1a10 10 0 01-2.2 6.29 1.58 1.58 0 01-.12.15l-.64.71c-2 2.11-6.9 6.75-6.9 6.75l-1.17-3.84a10.1 10.1 0 1111-10.06z"
+      fill={props.fill}
+    />
+  </Svg>
+);
